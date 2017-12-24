62b6121a1813cd8da969f4cb46bb42c836bc55fc
aqueel <aqueelansari1@gmail.com>
2017-12-24 15:12:47 +0530

    initial commit

---
+++ src/main/java/com/aqu/springboot/SpringBootRestApiApp.java
@@ -0,0 +1,12 @@
+package com.aqu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.aqu.springboot"})
+public class SpringBootRestApiApp{
+    public static void main (String args[]){
+        SpringApplication.run(SpringBootRestApiApp.class);
+    }
+
+}
